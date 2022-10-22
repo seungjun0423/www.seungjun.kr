a5c29b22795e7e21c7c3c7e72542fbb60cb71781
seungjun0423 <sibonum@gmail.com>
2022-10-22 20:12:30 +0900

    delete menu and insert into main

--- seungjun/pages/components/Main.tsx
+++ seungjun/pages/components/Main.tsx
@@ -1,6 +1,18 @@
+import styles from'../../styles/Common.module.css'
+
+import Menu from './Menu'
+
 const Main = () => {
   return (
-    <div>
+    <div className={styles.MainContainer}>
+      <div className={styles.MenuWrapper}>
+        <h1 className={styles.MenuItem}>1</h1>
+        <h1 className={styles.MenuItem}>2</h1>
+        <h1 className={styles.MenuItem}>3</h1>
+      </div>
+        <div className={styles.MainWrapper}>
+          hi
+        </div>
     </div>
   )
 }
