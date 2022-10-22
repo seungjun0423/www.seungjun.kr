d4d11cfe9b9b10ad51ac31142adfca0f3eee586f
seungjun0423 <sibonum@gmail.com>
2022-10-22 16:02:35 +0900

    add manifest.json

--- seungjun/next.config.js
+++ seungjun/next.config.js
@@ -1,7 +1,33 @@
 /** @type {import('next').NextConfig} */
+
+const withPlugins = require("next-compos-plugins");
+
+const withPWA = require("next-pwa")
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = withPlugins(
+	[
+		[
+			withPWA,
+			{
+				pwa: {
+					dest: "public",
+				},
+			},
+		],
+        [
+            typescript, 
+            {
+                typescriptLoaderOptions: {
+                    transpileOnly: false,
+                },
+            }
+        ],
+        // ...
+	],
+	nextConfig
+);
