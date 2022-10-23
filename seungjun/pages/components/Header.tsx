fd059b144c60c57fbb1da0fed8d88ec0c47e5b9f
seungjun0423 <sibonum@gmail.com>
2022-10-24 06:02:16 +0900

    add image

--- seungjun/pages/components/Header.tsx
+++ seungjun/pages/components/Header.tsx
@@ -1,10 +1,12 @@
 import styles from'../../styles/Common.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
 
 
 const Header = () => {
   return (
     <div className={styles.HeaderWrapper}>
+      <Image src="/icon-192x192.png" width={100} height={100}/> 
       <h1><Link href="/">Seungjun Lee</Link></h1>
     </div>
   )
