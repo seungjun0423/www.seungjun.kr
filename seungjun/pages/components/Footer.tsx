361bd26e4be648db22b3d005bd4774a67a9ca080
seungjun0423 <sibonum@gmail.com>
2022-10-25 07:28:32 +0900

    add css Footer.tsx

--- seungjun/pages/components/Footer.tsx
+++ seungjun/pages/components/Footer.tsx
@@ -8,10 +8,10 @@ const Footer = () => {
       <a href='sibonum@gmail.com' className={styles.FooterIcon}>
       <Image src="/email.png" width={55} height={60}></Image>
       </a>
-      <a href='https://github.com/seungjun0423'>
+      <a href='https://github.com/seungjun0423' className={styles.FooterIcon}>
         <Image src="/github.png" width={60} height={60}></Image>
       </a>
-      <a href='https://seungjun0423.notion.site'>
+      <a href='https://seungjun0423.notion.site' className={styles.FooterIcon}>
         <Image src="/notion.png" width={60} height={60}></Image>
       </a>
     </div>
