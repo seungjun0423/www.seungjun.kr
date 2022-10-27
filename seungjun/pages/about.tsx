3a0e5287c5b15c184bf6a5b567ea203201075c71
seungjun0423 <sibonum@gmail.com>
2022-10-27 20:01:26 +0900

    delete cursor css

--- seungjun/pages/about.tsx
+++ seungjun/pages/about.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 const about = () => {
   return (
-    <div>
+    <div className={styles.AboutWrapper}>
       <Header></Header>
       <div className={styles.AboutName}>
         <h1>이승준(1994.04.23~)</h1>
@@ -14,7 +14,7 @@ const about = () => {
         <div className={styles.AboutElement}>
           <h2>학력</h2>
           <br />
-          <div className={styles.contents1}>
+          <div>
             점촌고등학교 <br />(2010.03~2012.12) <br />
             국민대학교 법학부 사법학 전공<br />(2013.03~2021.08) <br />
             코드 스테이츠 부트캠프 소프트엔지니어링 과정<br />(2022.01~2022.06)
@@ -24,7 +24,7 @@ const about = () => {
         <div className={styles.AboutElement}>
           <h2>이력</h2>
           <br />
-          <div className={styles.contents2}>
+          <div>
             공군 병장 만기 제대 <br />(2015.01~2017.01) <br />
             구로구청 공공근로 인턴십<br/> (2020.06~2020.12) <br />
             에피넷 FE <br />(2022.09.19 ~ )
@@ -34,7 +34,7 @@ const about = () => {
         <div className={styles.AboutElement}>
           <h2>기술</h2>
           <br />
-          <div className={styles.contents3}>
+          <div>
             <span>JavaScript, React.js, Node.JS, Git, Python</span>
           </div>
         </div>
