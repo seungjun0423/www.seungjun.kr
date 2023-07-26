f24d9b8e135ec84cecd32a0f098fb71d6f692aad
seungjun0423 <sibonum@gmail.com>
2023-07-26 11:53:06 +0900

    globals.css && layout.module.css 생성

--- nextapp/src/app/layout.tsx
+++ nextapp/src/app/layout.tsx
@@ -1,3 +1,5 @@
+import './globals.css';
+import styles from './layout.module.css';
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,7 +16,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+			<head />
       <body >
+				<header>
+					<nav>
+						<h1>seungjun blog</h1>
+						<a href=''>Contact</a>
+						<a href=''>About</a>
+					</nav>
+				</header>
 				{children}
 			</body>
     </html>
