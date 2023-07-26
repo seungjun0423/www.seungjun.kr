720b22939946459260bcf51ee7f84e3f0dc81516
seungjun0423 <sibonum@gmail.com>
2023-07-26 10:27:43 +0900

    [slug]폴더에 page.tsx 추가

--- nextapp/src/app/study/[slug]/page.tsx
+++ nextapp/src/app/study/[slug]/page.tsx
@@ -10,12 +10,13 @@ export default function Post( { params }: Props ) {
   return (
     <div>
 			<h1>
-				this is post
+				{params.slug}this is study
 			</h1>
     </div>
   )
 }
 
 export function generateStaticParams() {
-
+	const category = ['html', 'css', 'javaScript', 'react', 'next.js', 'nest.js', 'algorithm'];
+	return category.map( (el) => ({ slug: el,}));
 }
---
+++ nextapp/src/app/study/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function Post( ) {
+  return (
+    <div>
+			<h1>
+				this is study
+			</h1>
+    </div>
+  )
+}
