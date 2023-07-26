181364ffc2fff42d39ffd90d23a56ce0be89284e
seungjun0423 <sibonum@gmail.com>
2023-07-27 05:26:07 +0900

    layout 수정 및 메타데이터 추가

--- nextapp/src/app/layout.tsx
+++ nextapp/src/app/layout.tsx
@@ -1,12 +1,19 @@
+import { Metadata } from 'next';
 import '../styles/globals.css';
 import styles from '../styles/layout.module.css';
 import { Inter } from 'next/font/google'
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Seungjun',
-  description: 'Hi',
+export const metadata: Metadata = {
+  title: '이승준의 개발 블로그',
+  description: '웹 개발자, 리액트 개발자, 노드 개발자, 프론트엔드 개발자, 풀스택 개발자, 블록체인 개발자, 비전공자 개발자, web developer, react developer, node developer, fronted developer, fullstack developer, blockchain developer',
+	
+	// Todo: 디자인 작업하면서 파비콘 추가하기
+	// icons: {
+	// 	icon: favicon,
+	// },
 }
 
 export default function RootLayout({
@@ -19,9 +26,10 @@ export default function RootLayout({
 			<head />
       <body >
 				<header className={styles.header}>
-					<h1>Blog</h1>
-					<nav className={styles.nav}>
-					</nav>
+					<Link className={styles.title} href={'/'}>Blog</Link>
+					<Link className={styles.nav} href={'/login'}>
+						login
+					</Link>
 				</header>
 				
 				<div className={styles.main}>
@@ -42,7 +50,9 @@ export default function RootLayout({
 				</div>
 				
 				<footer className={styles.footer}>
-					풋터입니다
+					<text>
+						풋터입니다
+					</text>
 				</footer>
 			</body>
     </html>
---
+++ nextapp/src/app/login/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Login() {
+  return (
+    <div>
+			login
+    </div>
+
+  )
+}
