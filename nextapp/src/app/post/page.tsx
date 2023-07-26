a0c7681e767e8fb54ae0fc06d948a3bd6c16abc5
seungjun0423 <sibonum@gmail.com>
2023-07-26 10:03:23 +0900

    동적 라우팅을 위한 [slug] 추가

--- nextapp/src/app/post/page.tsx
+++ nextapp/src/app/post/page.tsx
@@ -1,12 +1,18 @@
 'use client';
 
-export default function Post() {
+type Props = {
+	params: {
+		slug: string;
+	};
+};
+
+export default function Post( { params }: Props ) {
   return (
     <div>
 			<h1>
 				this is post
 			</h1>
     </div>
-
   )
 }
+
