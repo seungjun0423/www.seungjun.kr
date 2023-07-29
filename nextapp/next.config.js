884a90a72d28a052222e68ecb2ae2bf3cfaede33
seungjun0423 <sibonum@gmail.com>
2023-07-29 09:14:27 +0900

    빌드 에러 해결

--- nextapp/next.config.js
+++ nextapp/next.config.js
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				// Todo : 호스트 네임 추후 설정 필요 
-				hostname: '추후 설정'
-			}
-		]
-	},
-	async redirects(){
-		return [
-			{
-				// Todo : 추후 리다이렉트 설정 추가 필요
-			}
-		]
-	}
+	// images: {
+	// 	remotePatterns: [
+	// 		{
+	// 			protocol: 'https',
+	// 			// Todo : 호스트 네임 추후 설정 필요 
+	// 			hostname: '추후 설정'
+	// 		}
+	// 	]
+	// },
+	// async redirects(){
+	// 	return [
+	// 		{
+	// 			// Todo : 추후 리다이렉트 설정 추가 필요
+	// 		}
+	// 	]
+	// }
 };
 
 module.exports = nextConfig
