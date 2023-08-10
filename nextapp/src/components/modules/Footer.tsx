f177c392970d87a3d6a79ab59a9d9cdef8b257e9
이승준 <87581422+seungjun0423@users.noreply.github.com>
2023-08-11 08:14:13 +0900

    Deploy Test (#14)
    * Modify: next.config.js 에서발생하는 파싱에러 파악 및 해결(eslint 이슈)
    
    * Add: tsconfig.json 옵션 추가 (strictNullCheck, noImplicityAny)
    
    * Fix: 전역변수로 관리하던 반응형 상태를 변경
    
    * Remove: width 관련 zustand 주석처리
    
    * Fix: 컨텐츠 컴포넌트 주석처리 해제
    
    * Fix: 모바일 구현시최적화를 위해 조건부 렌더링을 사용하려 했으나 미디어 쿼리 사용(이유: 초기 width 값을 가져오는데 어려움이 있었다)
    
    * Modify: 미디어 쿼리 분기점 추가
    
    * Remove: Underbar 컴포넌트 삭제(이유: 상태를 공유하는 하나의 컴포넌트로 반응형 대응을 위함)
    
    * Fix: 반응형 css 수정
    
    * Modify: Header컴포넌트 css 수정
    
    * Fix: 반응형 수정(이유: 배포 확인 결과 모바일 환경에서반응형이 제대로 작동하지 않음)

--- nextapp/src/components/modules/Footer.tsx
+++ nextapp/src/components/modules/Footer.tsx
@@ -4,13 +4,14 @@ import React from "react";
 import styled from 'styled-components';
 
 const Footers = styled.footer`
+	bottom: 0;
 	display: flex;
 	width: 100%;
 	font-size: 1rem;
-	bottom: 0px;
 	border-top: 1px solid;
 	flex-direction: row-reverse;
 	align-content: center;
+
 	@media(max-width: 520px){
 		display: none;
 	}
@@ -20,6 +21,7 @@ const CopyRight = styled.p`
 	margin: 1.5rem;
 `;
 
+/** width 값이 520px 미만인 디바이스에선 가려진다 */
 export default function Footer(): React.ReactElement {
 	return (
 		<Footers>
