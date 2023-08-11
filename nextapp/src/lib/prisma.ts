e5144e6689128739692a785060983d891ed7352d
seungjun0423 <sibonum@gmail.com>
2023-08-11 17:48:56 +0900

    Modify: prisma.ts 개발버전 환경설정

--- nextapp/src/lib/prisma.ts
+++ nextapp/src/lib/prisma.ts
@@ -8,4 +8,4 @@ export const prisma =
     log: ["query"],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "development") globalForPrisma.prisma = prisma;
