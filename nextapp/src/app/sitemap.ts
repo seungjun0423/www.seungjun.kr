a65c07f97531cfc3fbd6af5c7c1e8addc3d2720f
seungjun0423 <sibonum@gmail.com>
2023-08-14 15:59:48 +0900

    Add: robots.ts, sitemap.ts 코드 작성

---
+++ nextapp/src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://seungjun.kr',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://seungjun.kr',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://seungjun.kr',
+      lastModified: new Date(),
+    },
+  ]
+}
