f05a717d7914ad312adc688916074a8c548c1465
seungjun0423 <sibonum@gmail.com>
2023-08-14 11:26:52 +0900

    Remove: prisma 파일, 폴더,스키마 제거 & store.ts 삭제

--- nextapp/src/context/store.ts
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { create } from 'zustand'
-// import { Width } from './interface';
-
-// export const InnerWidthStore = create<Width>((set) => ({
-//   innerWidth: -1,
-//   setInnerWidth: (input) => set( state => ({ innerWidth: input })),
-// }));
