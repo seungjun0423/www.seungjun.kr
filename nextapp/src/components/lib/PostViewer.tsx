efa238a2668bef5cb51a6569df3b6f33dc17e3d7
seungjun0423 <sibonum@gmail.com>
2023-08-15 13:51:31 +0900

    Add: PostViewer 컴포넌트 작업 완료 향후 데이터 넣어서 테스트 해볼것

--- nextapp/src/components/lib/PostViewer.tsx
+++ nextapp/src/components/lib/PostViewer.tsx
@@ -2,10 +2,18 @@
 
 import React from "react";
 import styled from "styled-components";
+import { Viewer } from '@toast-ui/react-editor';
+import '@toast-ui/editor/dist/toastui-editor-viewer.css';
+
+
+const PostViewers = styled.div`
+
+`;
 
 export default function Postviewer() {
 	return (
-		<>
-		</>
+		<PostViewers id=''>
+			<Viewer></Viewer>
+		</PostViewers>
 	)
 }
