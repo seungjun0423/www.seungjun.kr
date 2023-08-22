e7516436ad5a3138f9a31e45042d3980c1ed4fd1
seungjun0423 <sibonum@gmail.com>
2023-08-22 20:29:02 +0900

    Test: 배포테스트를 위한 임시 주석처리

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -8,9 +8,9 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     await this.$connect();
   }
   // 에러 발생시 디비 연결 해제
-  async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
-  }
+  // async enableShutdownHooks(app: INestApplication) {
+  //   this.$on('beforeExit', async () => {
+  //     await app.close();
+  //   });
+  // }
 }
