1f434256ba46382f72c0e05e115f1b7cd3f48b0a
이승준 <87581422+seungjun0423@users.noreply.github.com>
2023-08-23 03:18:11 +0900

    2023.08.23(수) serverNest (#37)
    * Add: Dockerfile 추가
    
    * Add: .dockerignore 추가
    
    * Add: .gitignore .env 추가
    
    * Add: Dockerfile 명령어 추가
    
    * Modify: 서버 배포를 위한 스크립트 변경
    
    * Remove: Dockerfile 제거
    
    * Test: 배포테스트를 위한 임시 주석처리
    
    * Delete .env
    
    * Modify: db 스키마 변경
    
    * Modify: edit 폴더 및 파일 삭제, Add: category 폴더 및 파일 추가
    
    * Remove: admin 폴더 및 파일들 삭제 스키마 변경
    
    * Add: auth 폴더 및 파일들 추가
    
    * Add: category post 요청과 get 요청 임시 구성
    
    * Add: PrismaService 카테고리 모듈에 추가
    
    * Fix: 카테고리 추가 데이터 타입 수정

---
+++ server/src/category/category.controller.ts
@@ -0,0 +1,22 @@
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CategoryService } from './category.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Category } from '@prisma';
+
+@Controller('category')
+export class CategoryController {
+  constructor(
+    private prismaService: PrismaService,
+    private categoryService: CategoryService,
+  ) {}
+
+  @Post('/creatCategory')
+  async createCategory(@Body() data: { title: string }): Promise<Category> {
+    return await this.categoryService.createCategory(data);
+  }
+
+  @Get()
+  async categoryList(): Promise<Category[]> {
+    return await this.categoryService.categoryList();
+  }
+}
---
+++ server/src/category/category.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { Category } from '@prisma';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class CategoryService {
+  constructor(private prismaService: PrismaService) {}
+
+  async createCategory(data: { title: string }): Promise<Category> {
+    return await this.prismaService.category.create({
+      data: { title: data.title },
+    });
+  }
+
+  async categoryList(): Promise<Category[]> {
+    return this.prismaService.category.findMany();
+  }
+}
