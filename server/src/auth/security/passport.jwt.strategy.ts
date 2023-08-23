f4b713cc657211adb06ebf94f2d8fea9ddd28045
이승준 <87581422+seungjun0423@users.noreply.github.com>
2023-08-23 17:37:33 +0900

    Add: auth 기능을 위한 기본 셋업

---
+++ server/src/auth/security/passport.jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy, VerifiedCallback } from "passport-jwt";
+import { AuthService } from "../auth.service";
+import { LoginPayload } from "./LoginPayload.interface";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy){
+    constructor(private readonly authService: AuthService){
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: true,
+            secretOrKey: 'needmoremoney'
+        })
+    }
+
+    async validate(payload: LoginPayload, done: VerifiedCallback): Promise<any> {
+        const user = await this.authService.tokenValidateUser(payload);
+				
+        if(!user) {
+            return done(new UnauthorizedException({message: 'user data cannot found'}), false);
+        }
+        return done(null, user);
+    }
+}
