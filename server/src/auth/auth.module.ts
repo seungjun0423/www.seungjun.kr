2215f17d15d0179155b4d9abafd24dc57b45f4b5
이승준 <87581422+seungjun0423@users.noreply.github.com>
2023-08-31 11:49:04 +0900

    Test: 시크릿 키 스트링 변경

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -9,7 +9,7 @@ import { PassportModule } from '@nestjs/passport';
 @Module({
   imports: [
     JwtModule.register({
-      secret: process.env.SECRET_KEY,
+      secret: `${process.env.SECRET_KEY}`,
       signOptions: { expiresIn: '300m' },
     }),
     PassportModule.register({ defaultStrategy: 'jwt' }),
