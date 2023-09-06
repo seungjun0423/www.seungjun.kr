5d90b309c41440b4bc05276032008fde4fee560d
이승준 <87581422+seungjun0423@users.noreply.github.com>
2023-09-07 05:35:11 +0900

    Fix: robot.txt 수정

--- nextapp/src/app/robots.ts
+++ nextapp/src/app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/public/',
     },
     sitemap: 'https://seungjun.kr/sitemap.xml',
   }
