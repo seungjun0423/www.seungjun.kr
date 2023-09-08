e9aae4e293e2b85eea3c9ae60b7e71af99bd89f9
이승준 <87581422+seungjun0423@users.noreply.github.com>
2023-09-08 21:24:32 +0900

    Add: max-width 추가 및 자동 중앙 정렬

--- nextapp/src/components/layouts/Content.tsx
+++ nextapp/src/components/layouts/Content.tsx
@@ -6,12 +6,18 @@ import { Props } from "app/layout";
 
 const Contents = styled.section`
 	width: 100%;
+	max-width: 1200px;
 	height: 100vh;
 	overflow-x: auto;
 	overflow-y: auto;
-	padding: 4rem;
+	padding: 4vw;
 	margin-bottom: 5vh;
 	/* font-family: initial; */
+
+	@media (min-width: 1200px){
+		margin-right: auto;
+		margin-left: auto;
+	}
 
 	@media (max-width: 576px) {
 		margin-bottom: 10vh;
