5d0338e9f7de83f15ba6fcbefe40d874f071957f
이승준 <87581422+seungjun0423@users.noreply.github.com>
2023-09-22 01:53:49 +0900

    Test : 쿠키 전송 에러 테스트8

--- server/src/middleware/logger.middleware.ts
+++ server/src/middleware/logger.middleware.ts
@@ -6,7 +6,7 @@ export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     console.log('리퀘스트 쿠키', req.cookies);
     // console.log('리퀘스트 헤더', req.headers);
-		console.log('리스폰스', res);
+		// console.log('리스폰스', res);
     next();
   }
 }
