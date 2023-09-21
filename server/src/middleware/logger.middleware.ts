2b66a84387233a78bc2eb55f4122442405bc74e4
이승준 <87581422+seungjun0423@users.noreply.github.com>
2023-09-22 00:40:23 +0900

    Test: 프로덕트에 쿠키 전송 에러 테스트4

--- server/src/middleware/logger.middleware.ts
+++ server/src/middleware/logger.middleware.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    // console.log('리퀘스트 쿠키', req.cookies);
+    console.log('리퀘스트 쿠키', req.cookies);
     // console.log('리퀘스트 헤더', req.headers);
 		// console.log('리스폰스', res.);
     next();
