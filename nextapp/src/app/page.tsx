5bad02927235c94dd1d8bc4db18e403df8f47577
이승준 <87581422+seungjun0423@users.noreply.github.com>
2023-11-05 23:52:45 +0900

    github api 설정 완료

--- nextapp/src/app/page.tsx
+++ nextapp/src/app/page.tsx
@@ -2,8 +2,21 @@ import Link from "next/link";
 import { FaGithubSquare, FaInstagram } from "react-icons/fa";
 import { FiMail } from "react-icons/fi";
 import styles from 'styles/home.module.css';
+import { Octokit } from "octokit";
 
-export default function Home() {
+export default async function Home() {
+	const octokit = new Octokit({
+		auth: process.env.NEXT_GITHUB_TOKEN
+	});
+	// const data = await octokit.request("GET /repos/{owner}/{repo}/issues", {
+	// 	owner: "seungjun0423",
+	// 	repo: "forFunClient",
+	// });
+	const data = await octokit.request('GET /users/{username}/repos', {
+		username: "seungjun0423",
+		page: 1
+	});
+	console.log("여기 부터 시작입니다",data.data);
   return (
     <section key='home' className={styles.home}>
 			<div className={styles.box}>
