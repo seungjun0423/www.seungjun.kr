5556a5360301f46c8767f804153c023524298eb6
이승준 <87581422+seungjun0423@users.noreply.github.com>
2024-02-25 11:29:55 +0900

    start demo

---
+++ nextapp/src/app/demo/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Demo  {
+	params: {
+		slug: string;
+	};
+};
+
+export default function Demo() {
+  return (
+		<section key='Demo'>
+			demo
+		</section>
+  )
+};
